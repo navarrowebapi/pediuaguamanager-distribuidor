0d2019ad6b9e5b445770310c9df4e5709a355206
navarro.fabio@gmail.com <navarro.fabio@gmail.com>
2018-11-27 11:59:26 -0200

    deploy3

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import VueRouter from "vue-router";
 import Auth from "./components/Auth.vue";
-import AuthSuccess from "./components/AuthSuccess.vue";
-import ViewContact from './components/ViewContact'
-import NewContact from './components/NewContact'
+import AuthSuccess from "../src/components/AuthSuccess.vue";
+import ViewContact from '../src/components/ViewContact.vue'
+import NewContact from '../src/components/NewContact.vue'
 
 const router = new VueRouter({
     mode: 'history',
